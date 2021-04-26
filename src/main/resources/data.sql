4e785e85d3b556821d1be2221c90998387cb4c15
Sophie Sobczak <ssobczak@norsys.fr>
2021-04-26 22:27:59 +0200

    Init data on startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT INTO address VALUES
+ ('1', 'Avenue de Disneyland'),
+ ('2', 'Rue de Donald Duck'),
+ ('3', 'Impasse des 101 dalmatiens'),
+ ('4', 'Boulevard de Walt');
+
+INSERT INTO house VALUES
+ ('1', 'la maison de Mickey', '1'),
+ ('2', 'La duck''s house', '2'),
+ ('3', 'Le livre de la jungle', '3'),
+ ('4', 'Le coffre fort', '4');
+
+INSERT INTO inhabitant VALUES 
+ ('1', 50, 'Mickey', '1'),
+ ('2', 48, 'Minnie', '1'),
+ ('3', 20, 'Donald', '2'),
+ ('4', 20, 'Pluto','2'),
+ ('5', 40, 'Mowgli', '3'),
+ ('6', 70, 'Picsou', '4'),
+ ('7', 9, 'Riri', '4'),
+ ('8', 10, 'Fifi', '4'),
+ ('9', 11, 'Loulou', '4');
+
+INSERT INTO  furniture VALUES
+ ('1', 'fauteuil', 100, '1'),
+ ('2', 'niche', 200, '2'),
+ ('3', 'lit', 300, '3')
