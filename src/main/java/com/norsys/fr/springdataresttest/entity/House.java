70ca23b2440df84b9ebdfb467b22fc4a0fb74a6a
BaptisteThery <baptiste1thery@gmail.com>
2021-05-18 18:18:35 +0200

    ajout last modified

--- src/main/java/com/norsys/fr/springdataresttest/entity/House.java
+++ src/main/java/com/norsys/fr/springdataresttest/entity/House.java
@@ -1,8 +1,10 @@
 package com.norsys.fr.springdataresttest.entity;
 
 import lombok.Data;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
 
@@ -15,6 +17,9 @@ public class House {
 
     @Version
     Long version;
+
+    @LastModifiedDate
+    Date date;
 
     String name;
     
--- src/main/java/com/norsys/fr/springdataresttest/entity/HouseProjection.java
+++ src/main/java/com/norsys/fr/springdataresttest/entity/HouseProjection.java
@@ -1,7 +1,5 @@
 package com.norsys.fr.springdataresttest.entity;
 
-import com.norsys.fr.springdataresttest.entity.Furniture;
-import com.norsys.fr.springdataresttest.entity.House;
 import org.springframework.data.rest.core.config.Projection;
 
 import java.util.List;
